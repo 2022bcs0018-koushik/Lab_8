4b5380fa9ba5fe02b7ee1f68e2811d6082f1a90a
Koushik <vvkoushik2004@gmail.com>
2026-04-11 02:01:49 +0530

    stop tracking housing.csv

--- data/modify.py
+++ data/modify.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
 import pandas as pd
-df = pd.read_csv("./data/housing.csv")
+
+
+data_file = Path(__file__).with_name("housing.csv")
+df = pd.read_csv(data_file)
 df_v1 = df.head(5000)
-df_v1.to_csv("data/housing.csv", index=False)
+df_v1.to_csv(data_file, index=False)
